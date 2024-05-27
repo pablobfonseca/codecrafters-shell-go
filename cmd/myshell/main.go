3204a5e57f1d00b60276601e44a668f168fa7713
Pablo Fonseca <pablofonseca777@gmail.com>
2024-05-27 12:12:23 +0100

    feat: implement exit command

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -11,10 +11,22 @@ func main() {
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
 
-		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		in, err := bufio.NewReader(os.Stdin).ReadString('\n')
 
 		if err != nil {
 			fmt.Println(err.Error())
+		}
+
+		command := strings.TrimRight(in, "\n")
+		cmd_with_args := strings.Split(command, " ")
+
+		command = cmd_with_args[0]
+		args := cmd_with_args[1:]
+
+		if command == "exit" {
+			if args[0] == "0" {
+				os.Exit(0)
+			}
 		}
 
 		fmt.Printf("%s: command not found\n", strings.TrimRight(command, "\n"))
